d44c013098b0ee1ea774f254ad0b378d1c3cdd24
Kasif Khan <kasif780780@gmail.com>
2018-04-01 00:13:54 +0530

    Enumerable.Where implementation

---
+++ LinkP6/LinkP6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinkP6
+{
+    class Program
+    {
+        //Where returns the elements from the input sequence that satisfy the given predicate
+        static void Main(string[] args)
+        {
+            string[] names = { "Asif", "Tom", "Jeery", "Merry", "Dick" };
+            IEnumerable<string> query = names.Where(n => n.EndsWith("y"));
+
+            foreach(string name in query)
+            {
+                Console.WriteLine(name);
+                Console.ReadLine();
+            }
+        }
+    }
+}
