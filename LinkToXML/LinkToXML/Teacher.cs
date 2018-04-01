7c9821c8e2c3d0b09aced6438c5c2f63ee50ef9c
Kasif Khan <kasif780780@gmail.com>
2018-04-01 19:13:30 +0530

    Link To XML

---
+++ LinkToXML/LinkToXML/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqTutorial
+{
+    class Teacher
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public int ID { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ LinqTutorial/LinqTutorial/LinkToXML.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace LinqTutorial
+{
+    class LinkToXML
+    {
+        static void Main()
+        {
+            //Create The Data Source by Using a Collection Initilizer
+            //The Student Class was defined
+
+            List<Student> students = new List<Student>()
+            {
+                new Student {
+                    First ="Svetlana",
+                    Last ="Omelchenko",
+                    ID =111,
+                    Scores = new List<int>{97, 92, 81, 60}},
+                new Student {
+                    First ="Claire",
+                    Last ="O’Donnell",
+                    ID =112,
+                    Scores = new List<int>{75, 84, 91, 39}},
+                new Student {
+                    First ="Sven",
+                    Last ="Mortensen",
+                    ID =113,
+                    Scores = new List<int>{88, 94, 65, 91}},
+
+            };
+
+            //Create The Query
+
+            var studentToXML = new XElement("Root", from std in students
+
+                                                    let x = string.Format("{0}", "{1}", "{2}", std.Scores[3])
+                                                    select new XElement("std",
+                                                    new XElement("First", std.First),
+                                                     new XElement("First", std.Last),
+                                                     new XElement("Score", x)
+                                                     )
+                    );
+
+            Console.WriteLine(studentToXML);
+            //Execute The Query
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
