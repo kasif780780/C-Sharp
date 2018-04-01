5c0351df0be51c05a7522980758044a1722f2dd3
Kasif Khan <kasif780780@gmail.com>
2018-04-01 19:32:39 +0530

    Performing Operations on Source Elements

---
+++ LinQTutorial_2/LinQTutorial_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace LinQTutorial_2
+{
+    class Program
+    {
+        //Performing Operations on Source Elements
+
+        static void Main(string[] args)
+        {
+            //Data Source
+            double[] reddi = { 1, 2, 3 };
+
+
+            //Query
+
+            IEnumerable<string> query = from rad in reddi select string.Format("Area={0}", (rad) * (rad) * 3.14);
+
+            //Query Execution
+
+
+            foreach(string s in query)
+            {
+                Console.WriteLine(s);
+            }
+
+
+            Console.WriteLine("Press Any Key To Exit");
+            Console.ReadKey();
+        }
+    }
+}
