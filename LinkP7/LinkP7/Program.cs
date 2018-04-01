e6ba7ffc326fecdd2d18ca5e9cf18b1346bbb4c2
Kasif Khan <kasif780780@gmail.com>
2018-04-01 11:57:22 +0530

    TakeWhile and SkipWhile Operator

---
+++ LinkP7/LinkP7/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinkP7
+{
+   //TakeWhile and SkipWhile link Operator
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 3, 5, 2, 234, 4,1 };
+            var takewhilesmall = number.TakeWhile(n => n <100);
+            Console.WriteLine("TakeWhile Result");
+            Console.WriteLine("==================================");
+            foreach(int nm in takewhilesmall)
+
+            {
+                Console.WriteLine(nm);
+                Console.ReadLine();
+            }
+            int[] number1 = { 3, 5, 2, 234, 4, 1 };
+            var skipwhilesmall = number1.SkipWhile(n => n < 100);
+            Console.WriteLine("TakeWhile Result");
+            Console.WriteLine("==================================");
+            foreach (int nm1 in skipwhilesmall)
+            {
+                Console.WriteLine(nm1);
+                Console.ReadLine();
+            }
+        }
+    }
+}
