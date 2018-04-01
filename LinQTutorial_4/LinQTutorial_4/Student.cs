74b871f8629693878eb4e48ec8f576a333d827fa
Kasif Khan <kasif780780@gmail.com>
2018-04-01 20:17:28 +0530

    Walkthrough: Writing Queries in C# (LINQ)

---
+++ LinQTutorial_4/LinQTutorial_4/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace LinQTutorial_4
+{
+    class Student
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public int ID { get; set; }
+        public List<int> Scores;
+    }
+
+   
+}
