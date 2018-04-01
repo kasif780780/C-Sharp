25ea649760f9c9a4a4d443cda4c8fade091a41e8
Kasif Khan <kasif780780@gmail.com>
2018-04-01 18:32:12 +0530

    Data Transformation with Linq

--- LinqTutorial/LinqTutorial/IntroToLinq.cs
+++ LinqTutorial/LinqTutorial/IntroToLinq.cs
@@ -10,19 +10,63 @@ namespace LinqTutorial
         static void Main()
         {
             //The Three Parts Of LINQ Query
-            //1.Data Source
-            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+            //1. Create First Data Source 
+            List<Student> students = new List<Student>()
+            {
+                   new Student {
+                       First ="Svetlana",
+                       Last ="Omelchenko",
+                       ID =111,
+                       Street ="123 Main Street",
+                       City ="Seattle",
+                       Scores = new List<int> {97, 92, 81, 60}},
+                   new Student {First="Claire",
+                       Last ="O’Donnell",
+                       ID =112,
+                       Street ="124 Main Street",
+                       City ="Redmond",
+                       Scores = new List<int> {75, 84, 91, 39}},
+                    new Student {First="Sven",
+                        Last ="Mortensen",
+                        ID =113,
+                        Street ="125 Main Street",
+                        City ="Lake City",
+                        Scores = new List<int> {88, 94, 65, 91}},
+
+            };
+            //2 Create Second Data Source
+            List<Teacher> teachers = new List<Teacher>() {
+                new Teacher {
+                    First = "Ann",
+                    Last = "Beebe",
+                    ID = 945,
+                    City = "Seattle" },
+                new Teacher {
+                    First = "Alex",
+                    Last = "Robinson", ID = 956,
+                    City = "Redmond" },
+                new Teacher {
+                    First = "Michiyo",
+                    Last = "Sato",
+                    ID = 972,
+                    City = "Tacoma" }
+            };
 
             //2.Query Creation
-            var numQuery = from num in numbers where (num % 2) == 0 select num;
+            //  var numQuery = from num in numbers where (num % 2) == 0 select num;
 
+
+            var pepoleinSeattle = (from std in students where std.City == "Seattle" select std.Last)
+                .Concat(from tech in teachers where tech.City == "Seattle" select tech.Last);
+                
 
 
             //3.Query Execution
-            foreach(var sys in numQuery)
+            foreach (var sys in pepoleinSeattle)
             {
                 Console.WriteLine(sys);
             }
+            Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
     }
