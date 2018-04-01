daac55fda0643450be56e2880ec197d84238ae7e
Kasif Khan <kasif780780@gmail.com>
2018-04-01 18:05:32 +0530

    Introduction to Linq Query

---
+++ LinkP8/LinkP8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinkP8
+{
+    //Select Link Operator
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ LinqTutorial/LinqTutorial/IntroToLinq.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LinqTutorial
+{
+    class IntroToLinq
+    {
+        static void Main()
+        {
+            //The Three Parts Of LINQ Query
+            //1.Data Source
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+            //2.Query Creation
+            var numQuery = from num in numbers where (num % 2) == 0 select num;
+
+
+
+            //3.Query Execution
+            foreach(var sys in numQuery)
+            {
+                Console.WriteLine(sys);
+            }
+            Console.ReadKey();
+        }
+    }
+}
