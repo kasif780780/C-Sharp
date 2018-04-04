fbf29b9e12c2c5d09074acd038ab824f483e25a5
Kasif Khan <kasif780780@gmail.com>
2018-04-05 01:21:35 +0530

    Sorting Data

---
+++ LinQTutorial_5/LinQTutorial_5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LinQTutorial_5
+{
+    class Program
+    {
+        //Standard Query Operator
+
+        static void Main(string[] args)
+        {
+            string sentance = "the quik brown fox jumps over lagy dogs";
+
+            //Split the string into individual words to create a collection
+            string[] words = sentance.Split(' ');
+
+            // Using query expression syntax
+            var query = from word in words group word.ToUpper() by word.Length into gr orderby gr.Key
+                        select new { Length = gr.Key, Words = gr };
+
+            //Using Mathod Based Query syntax
+            var query2 = words.GroupBy(w => w.Length, w => w.ToUpper()).Select(g => new { Length = g.Key, Words = g }).OrderBy(o => o.Length);
+
+            foreach (var obj in query)
+            {
+                Console.WriteLine("Words of length {0}:", obj.Length);
+                foreach 
+                    (string word in obj.Words)
+                    Console.WriteLine(word);
+            }
+            Console.ReadKey( );
+        }
+    }
+}
---
+++ LinkTutorials_7/LinkTutorials_7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinkTutorials_7
+{
+
+    // Query Expression Syntax Examples 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  Primary Ascending Sort
+
+            string[] words = { "Asif", "Denim", "Akon", "Application" };
+         IEnumerable<string> Query = from word in words orderby word.Length select word;
+
+            foreach(var item in Query)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
