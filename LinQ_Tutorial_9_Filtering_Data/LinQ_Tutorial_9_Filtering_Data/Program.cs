c1fae77798008e4fff62928e0204cf6dfc3d04cc
Kasif Khan <kasif780780@gmail.com>
2018-04-07 01:21:03 +0530

    the select clause to project the first letter from each string in a list of strings

---
+++ LinQ_Tutorial_9_Filtering_Data/LinQ_Tutorial_9_Filtering_Data/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinQ_Tutorial_9_Filtering_Data
+{
+    class Program
+    {
+        //Filtering Data 
+        static void Main(string[] args)
+        {
+            string[] movies = { "Megamind", "BabyBoss", "Titanic", "TubeLight", "Ironman", "Spiderman", "Batman", "Xman" };
+
+            IEnumerable<string> Query= from movie in movies where movie.Length == 8 select movie;
+
+            foreach(var item in Query)
+            {
+                Console.WriteLine(item);
+               
+            }
+            Console.WriteLine("Press Any Key To Exit");
+                
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LinQ_Tutorials_9_Projection_Operation/LinQ_Tutorials_9_Projection_Operation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinQ_Tutorials_9_Projection_Operation
+{
+    class Program
+    {
+        //Projection Operation
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>() { "an", "apple", "a", "day" };
+            var query = from word in words select word.Substring(0, 1);
+            foreach(string item in query)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
