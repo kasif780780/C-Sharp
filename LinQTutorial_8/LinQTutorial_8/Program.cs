a3511435c706d12b7423795ddd0cbde75af5c865
Kasif Khan <kasif780780@gmail.com>
2018-04-07 00:52:00 +0530

    Distinct Sorting

---
+++ LinQTutorial_8/LinQTutorial_8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LinQTutorial_8_Set_Operation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Asif", "Asif", " Tom", "Jaery", "Vin", };
+          //  string[] names1 = { "Asif", "Asif", " Tom", "Jaery", "Vin", };
+
+            //Distinct Sorting
+            IEnumerable<string> query = from name in names.Distinct() orderby name.Length select name;
+            foreach(var item in query)
+            {
+
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
