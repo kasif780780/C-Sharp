abf3d5bf3a66d3f5aeb04e09749cc9e7b6ed2d67
Kasif Khan <kasif780780@gmail.com>
2018-04-07 00:25:45 +0530

    Primary Descending Sort

--- LinkTutorials_7/LinkTutorials_7/Program.cs
+++ LinkTutorials_7/LinkTutorials_7/Program.cs
@@ -13,12 +13,26 @@ namespace LinkTutorials_7
           //  Primary Ascending Sort
 
             string[] words = { "Asif", "Denim", "Akon", "Application" };
-         IEnumerable<string> Query = from word in words orderby word.Length select word;
+            string[] words1 = { "The", "Quic", "brown", "fox" ,"jump"};
+            IEnumerable<string> Query = from word in words orderby word.Length select word;
 
+            //Primary Descending
+            IEnumerable<string> Query1 = from word1 in words1 orderby word1.Substring(0, 3) descending select word1;
+            Console.WriteLine("==================Primary Asecending Sorting============");
             foreach(var item in Query)
             {
                 Console.WriteLine(item);
             }
+
+            Console.WriteLine("==================Primary Descending Sorting============");
+            foreach (var item1 in Query1)
+            {
+                Console.WriteLine(item1);
+            }
+
+
+           
+
             Console.ReadKey();
         }
     }
