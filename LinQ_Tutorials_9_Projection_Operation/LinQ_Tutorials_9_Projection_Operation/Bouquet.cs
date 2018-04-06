cc18be10b74d17bcf2514ee0d59aa1a76f05f5f0
Kasif Khan <kasif780780@gmail.com>
2018-04-07 01:39:16 +0530

    Select Vs SelectMany

---
+++ LinQ_Tutorials_9_Projection_Operation/LinQ_Tutorials_9_Projection_Operation/Bouquet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LinQ_Tutorials_9_Projection_Operation
+{
+    class Bouquet
+    {
+        public List<string> Flowers { get; set; }
+    }
+}
--- LinQ_Tutorials_9_Projection_Operation/LinQ_Tutorials_9_Projection_Operation/Program.cs
+++ LinQ_Tutorials_9_Projection_Operation/LinQ_Tutorials_9_Projection_Operation/Program.cs
@@ -7,14 +7,29 @@ namespace LinQ_Tutorials_9_Projection_Operation
     class Program
     {
         //Projection Operation
+        //Select Vs SelectMany
         static void Main(string[] args)
         {
-            List<string> words = new List<string>() { "an", "apple", "a", "day" };
-            var query = from word in words select word.Substring(0, 1);
-            foreach(string item in query)
+            List<Bouquet> bouquet = new List<Bouquet>()
             {
+                new Bouquet{Flowers=new List<string>{"SunFlower","Daisy","Daffodil","larkspur"}},
+                new Bouquet{Flowers=new List<string>{ "tulip", "rose", "orchid", "larkspur"}},
+                new Bouquet{Flowers=new List<string>{ "gladiolis", "lilac", "snapdragon", "aster","protea"}},
+                new Bouquet{Flowers=new List<string>{ "larkspur", "lily", "iris", "dahlia"}}
+
+            };
+            // *********** Select ***********       
+            IEnumerable<List<string>> query1 = bouquet.Select(bq => bq.Flowers);
+            // ********* SelectMany *********   
+            IEnumerable<string> query2 = bouquet.SelectMany(bq => bq.Flowers);  
+            Console.WriteLine("Results by using Select():");
+            // Note the extra foreach loop here.    
+            foreach (IEnumerable<String> collection in query1)
+                foreach (string item in collection)
+                    Console.WriteLine(item);  
+            Console.WriteLine("\nResults by using SelectMany():");
+            foreach (string item in query2)
                 Console.WriteLine(item);
-            }
             Console.ReadKey();
         }
     }
